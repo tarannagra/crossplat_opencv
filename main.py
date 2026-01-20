c1b22d5886d20e6844c87b7c417998659c55e7ca
tarannagra <tarannagra@proton.me>
2026-01-20 00:31:58 +0000

    made a change and let's see the performance difference

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import cv2
+import time
 import numpy as np
 
-def main():
-    print("OpenCV Version: ", end="")
-    print(cv2.__version__)
+# Create large image
+img = np.random.randint(0, 255, (4000, 4000, 3), dtype=np.uint8)
 
-
-if __name__ == "__main__":
-    main()
+start = time.time()
+for _ in range(100):
+    blurred = cv2.GaussianBlur(img, (15, 15), 0)
+print(f"Time: {time.time() - start:.2f}s")
